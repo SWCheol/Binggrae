ef3e537b900ee2f4f2329acfdf0bdab211c2c988
SWCheol <110226523+SWCheol@users.noreply.github.com>
2022-09-08 18:24:24 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,76 +1,22 @@
-// header
-let header = document.getElementById('header');
+// // browser width check for responsive web
+// let browserWidth = () => {
+//     body.removeAttribute('class');
+//     innerWidth > 1024 ? 
+//     header.classList.add('pc') :
+//     innerWidth > 768 ?
+//         header.classList.add('tablet') :
+//         header.classList.add('mobile'); 
+// }
 
-document.addEventListener('scroll', () =>{
-    scrollY >= 5 ? header.classList.add('white') :
-                    header.classList.remove('white');
-});
+// let body = document.querySelector('body');
+// let header = document.getElementById('header');
+// const scrollWidth = body.clientWidth;
+// // 햄버거 메뉴 누를 때 스크롤 가로 크기만큼 패딩 넣기
 
+// window.addEventListener('DOMContentLoaded', () => {
+//     browserWidth();
+// });
 
-// 글로벌 네비게이션 바
-let gnb = document.querySelector('.gnb');
-let gnbsub = document.querySelectorAll('.gnbsub');
-
-
-// 탑 메뉴
-gnb.addEventListener('mouseenter', () => {
-    if(!(header.classList.contains('hamburger' || 'mobile'))) {
-        console.log('pc!');
-        header.classList.add('white');
-        header.classList.add('dropDown');
-    
-        for(let index of gnbsub) {
-            index.style.display = 'flex';
-            index.classList.add('opacity');
-        }
-    
-        header.addEventListener('mouseleave',() => {
-            header.classList.remove('dropDown');
-            if(scrollY < 5) {
-                header.classList.remove('white');
-            }
-    
-            for(let index of gnbsub) {
-                index.style.display = 'none';
-                index.classList.remove('opacity');
-            }
-        });
-    }
-});
-
-
-// 햄버거 메뉴
-let hamburgerMenu = document.querySelector('.enb').childNodes[3].childNodes[1];
-let hamburger = document.querySelector('.hamburger');
-let body = document.getElementsByTagName('body')[0];
-
-
-hamburgerMenu.addEventListener('click',() => {
-    hamburger.classList.toggle('active');
-
-    if(body.scrollWidth > 1024) {
-        header.classList.toggle('hamburger');
-        body.style.overflow != 'hidden' ? body.style.overflow = 'hidden' :
-                                        body.style.overflow = 'auto';
-    } else {
-        header.classList.toggle('mobile');
-        body.style.overflow != 'hidden' ? body.style.overflow = 'hidden' :
-                                        body.style.overflow = 'auto';
-    }
-});
-
-
-// 모바일 메뉴
-
-
-// 언어 선택 메뉴
-let langChoice = document.querySelector('.langChoice');
-let langLink = document.querySelector('.langLink');
-
-langChoice.addEventListener('click',() => {
-    langLink.classList.toggle('active');
-});
-
-langLink.addEventListener('click',() => { 
-    langLink.classList.toggle('active');
-});
+// window.addEventListener('resize', () => {
+//     browserWidth();
+// });
---
+++ test.js
@@ -0,0 +1,6 @@
+let a;
+for(a = 0; a < 20; a++) {
+    console.log(a);
+}
+
+console.log('a는 ' + a);
