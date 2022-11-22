d8e19415a8ecda1697d30230d509b2bef74dce51
SWCheol <time3837@gmail.com>
2022-11-22 09:36:01 +0900

    css to scss integrate

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@
 //     // }
 // };
 
-
 // /* taste scroll event */
 // let taste = document.getElementById('taste');
 
@@ -28,48 +27,52 @@
 
 // window.addEventListener('scroll', listUp(community));
 
-
 /* slide event  */
-let itemSlide = function(slide) {
-    let btnPrev = slide.parentElement.nextElementSibling;
-    let btnNext = btnPrev.nextElementSibling;
+let itemSlide = function (slide) {
+  let btnPrev = slide.parentElement.nextElementSibling;
+  let btnNext = btnPrev.nextElementSibling;
 
-    slide.style.position = 'relative';
-    slide.style.right = 0;
+  slide.style.position = "relative";
+  slide.style.right = 0;
 
-    btnPrev.addEventListener('click', function() {
-        let liWidth = slide.children[0].clientWidth;
-        if(slide.style.right != '0px')
-            slide.style.right = parseFloat(slide.style.right) - parseFloat(liWidth) + 'px';
-    });
-
-    btnNext.addEventListener('click', function() {
-        let liWidth = slide.children[0].clientWidth;
-        if(slide.style.right != (slide.children.length - Math.round(slide.clientWidth / liWidth)) * liWidth + 'px')
-            slide.style.right = parseFloat(slide.style.right) + parseFloat(liWidth) + 'px';
-    });
-}
-
-let listRight = function(slide) {
-    console.log('console');
-    if(body.clientWidth <= 768) {
-        slide.style.right = '0px';
-        console.log('right zero!');
-    }
-
+  btnPrev.addEventListener("click", function () {
     let liWidth = slide.children[0].clientWidth;
-    // slide.style.right = parseFloat(liWidth) + 'px';
-}
+    if (slide.style.right != "0px")
+      slide.style.right =
+        parseFloat(slide.style.right) - parseFloat(liWidth) + "px";
+  });
 
+  btnNext.addEventListener("click", function () {
+    let liWidth = slide.children[0].clientWidth;
+    if (
+      slide.style.right !=
+      (slide.children.length - Math.round(slide.clientWidth / liWidth)) *
+        liWidth +
+        "px"
+    )
+      slide.style.right =
+        parseFloat(slide.style.right) + parseFloat(liWidth) + "px";
+  });
+};
+
+let listRight = function (slide) {
+  if (body.clientWidth <= 768) {
+    slide.style.right = "0px";
+    console.log("right zero!");
+  }
+
+  let liWidth = slide.children[0].clientWidth;
+  // slide.style.right = parseFloat(liWidth) + 'px';
+};
 
 /* taste list slide */
-let tasteList = document.querySelector('.tasteSlide .list');
+let tasteList = document.querySelector(".tasteSlide .list");
 
-window.addEventListener('DOMContentLoaded', itemSlide(tasteList));
-tasteList.addEventListener('resize', listRight(tasteList));
+window.addEventListener("DOMContentLoaded", itemSlide(tasteList));
+tasteList.addEventListener("resize", listRight(tasteList));
 
 /* shop list slide */
-let shopList = document.querySelector('.shopSlide .list');
+let shopList = document.querySelector(".shopSlide .list");
 
-window.addEventListener('DOMContentLoaded', itemSlide(shopList));
+window.addEventListener("DOMContentLoaded", itemSlide(shopList));
 // shopList.addEventListener('resize', listRight(shopList));
