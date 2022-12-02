752a2926ae5b2e63c223830ab6cc07a915198b8d
SWCheol <time3837@gmail.com>
2022-12-02 16:46:14 +0900

    site compressing for one html

--- page/company/js/ideology.js
+++
@@ -1,8 +0,0 @@
-let item = document.querySelectorAll('.item');
-
-// item.forEach(element => {
-//     element.style.height = item[1].clientHeight + 'px';
-// });
-
-item[0].style.height = item[1].clientHeight + 'px' 
-item[2].style.height = item[1].clientHeight + 'px';
--- test.js
+++
@@ -1,6 +0,0 @@
-let a;
-for(a = 0; a < 20; a++) {
-    console.log(a);
-}
-
-console.log('a는 ' + a);
